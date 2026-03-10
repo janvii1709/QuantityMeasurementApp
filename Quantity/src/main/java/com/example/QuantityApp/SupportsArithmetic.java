bb603d5afdd0eccf20f44e4874a0daf7af1be376
janvii1709 <janhavi.singh1709@gmail.com>
2026-03-10 15:16:23 +0530

    [Janhavi Singh]Add.adding the UC14.

---
+++ Quantity/src/main/java/com/example/QuantityApp/SupportsArithmetic.java
@@ -0,0 +1,7 @@
+package com.example.QuantityApp;
+
+@FunctionalInterface
+public interface SupportsArithmetic {
+    boolean isSupported();
+}
+
---
+++ Quantity/src/main/java/com/example/QuantityApp/WeightUnit10.java
@@ -0,0 +1,24 @@
+package com.example.QuantityApp;
+
+public enum WeightUnit10 implements IMeasurable {
+
+    KILOGRAM(1.0),
+    GRAM(0.001),
+    POUND(0.45359237);
+
+    private final double conversionFactor;
+
+    WeightUnit10(double conversionFactor) {
+        this.conversionFactor = conversionFactor;
+    }
+
+    @Override
+    public double getConversionFactor() {
+        return conversionFactor;
+    }
+
+    @Override
+    public String getUnitName() {
+        return name();
+    }
+}
